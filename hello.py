713c9ee1f436581837261b13906c88300944f237
Vivek D <vivster7@gmail.com>
2013-08-27 17:16:25 -0400

    added support for HTML pages

--- hello.py
+++ hello.py
@@ -1,10 +1,11 @@
-from flask import Flask 
-
-app = Flask(__name__)  
-
-@app.route("/")  
-def hello():  
-  return "Hello World!"  
+from flask import Flask  
+from flask import render_template  
   
+app = Flask(__name__)  
+  
+@app.route('/')  
+def hello(name=None):  
+  return render_template('hello.html')  
+    
 if __name__ == "__main__":  
-  app.run('0.0.0.0')  
+  app.run('0.0.0.0') 
