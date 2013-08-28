94892bd57fff710a0dc4aec2d590db6931c77693
Vivek D <vivster7@gmail.com>
2013-08-28 16:02:40 -0400

    added music playing code to hello.py

--- doorbell.py
+++ doorbell.py
@@ -1,4 +1,6 @@
 import pygame
+import time
 pygame.mixer.init()
 pygame.mixer.music.load('doorbell.mp3')
 pygame.mixer.music.play()
+time.sleep(20)
--- hello.py
+++ hello.py
@@ -1,21 +1,28 @@
 from flask import Flask  
 from flask import render_template  
-  
+import pygame
+import time
+
 app = Flask(__name__)  
   
 @app.route('/')  
-def hello(name=None):  
-  return render_template('hello.html') 
+def hello(name=None):
+  pygame.mixer.init()
+  pygame.mixer.music.load('doorbell.mp3')
+  pygame.mixer.music.play()
+  time.sleep(20)
 
-@app.route('/ring', methods=['POST'])
-def ring():
-  if request.method == 'POST':
-    # Get the value id
-    id1 = request.form['id']
-    print "---Message is", id1
-  else:
-    id1 = None
-  return render_template('index.html', value=id1)
+#   return render_template('hello.html') 
+
+# @app.route('/ring', methods=['POST'])
+# def ring():
+#   if request.method == 'POST':
+#     # Get the value id
+#     id1 = request.form['id']
+#     print "---Message is", id1
+#   else:
+#     id1 = None
+#   return render_template('index.html', value=id1)
     
 if __name__ == "__main__":  
   app.run('0.0.0.0') 
