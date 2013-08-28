fa7b8adf3f4f3a7963fb2f24557fe6d13f0f3607
Vivek D <vivster7@gmail.com>
2013-08-28 15:48:23 -0400

    play doorbell song with python

---
+++ doorbell.py
@@ -0,0 +1,4 @@
+import pygame
+pygame.mixer.init()
+pygame.mixer.music.load('doorbell.mp3')
+pygame.mixer.music.play()
--- hello.py
+++ hello.py
@@ -5,7 +5,18 @@ app = Flask(__name__)
   
 @app.route('/')  
 def hello(name=None):  
-  return render_template('hello.html')  
+  return render_template('hello.html') 
+
+@app.route('/ring', methods=['POST'])
+def ring():
+  if request.method == 'POST':
+    # Get the value id
+    id1 = request.form['id']
+    print "---Message is", id1
+  else:
+    id1 = None
+  return render_template('index.html', value=id1)
     
 if __name__ == "__main__":  
   app.run('0.0.0.0') 
+
